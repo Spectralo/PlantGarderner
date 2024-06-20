cf21afdcb327c8b2a2ca5f6373c9acf0913a4bb7
spectralo <arthur.hardy@mageekbox.net>
2024-06-20 18:41:59 +0200

    Tried to add MD3 theming

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,24 @@
-import { Stack } from "expo-router";
+import { useMaterial3Theme } from '@pchmn/expo-material3-theme';
+import { useColorScheme } from 'react-native';
+import {
+  MD3DarkTheme,
+  MD3LightTheme,
+  PaperProvider,
+} from 'react-native-paper';
+
+
 
 export default function RootLayout() {
+  const colorScheme = useColorScheme();
+  const { theme } = useMaterial3Theme();
+
+  const paperTheme =
+    colorScheme === 'dark'
+      ? { ...MD3DarkTheme, colors: theme.dark }
+      : { ...MD3LightTheme, colors: theme.light };
+
   return (
-    <Stack>
-      <Stack.Screen name="index" />
-    </Stack>
+    <PaperProvider theme={paperTheme}>
+    </PaperProvider>
   );
 }
